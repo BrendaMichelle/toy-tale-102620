5789364f4f96745589e8d85131afc5329b25e6d0
Michelle Rios <brendamichelle995@gmail.com>
2020-12-17 15:27:10 -0500

    after refactor

--- src/index.js
+++ src/index.js
@@ -13,49 +13,15 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
+  /*************** VARIABLES ************************/
   const url = 'http://localhost:3000/toys'
-  fetch(url)
-    .then(response => response.json())
-    .then(toys => {
-      toys.forEach(function (toyObject) {
-        renderNewToy(toyObject)
-        // ARTISINAL WAY
-        // const div = document.createElement('div')
-        // const h2 = document.createElement('h2')
-        // const img = document.createElement('img')
-        // const p = document.createElement('p')
-        // const button = document.createElement('button')
-
-        // div.classList.add('card')
-        // h2.textContent = toyObject.name
-        // img.src = toyObject.image
-        // // img.classList.add('toy-avatar')
-        // img.className = "toy-avatar"
-        // p.textContent = toyObject.likes
-        // button.className = "like-btn"
-        // button.textContent = "Likes <3"
-
-        // div.append(h2, img, p, button)
-
-        // const toyCollection = document.querySelector("#toy-collection")
-        // console.log(toyCollection)
-
-        // toyCollection.append(div)
-
-      })
-    })
-
   const newToyForm = document.querySelector('.add-toy-form')
+  const toyCollectionDiv = document.querySelector('#toy-collection')
+
+
+  /*************** EVENT LISTENERS ************************/
   newToyForm.addEventListener('submit', function (event) {
     event.preventDefault()
-    console.log(event.target)
-    const nameInput = event.target.name.value
-    const imageInput = event.target.image.value
-    const newToyObject = {
-      name: nameInput,
-      image: imageInput,
-      likes: 0
-    }
 
     fetch(url, {
       method: 'POST',
@@ -63,50 +29,56 @@ document.addEventListener("DOMContentLoaded", () => {
         "Content-Type": "application/json",
         "Accept": "application/json"
       },
-      body: JSON.stringify(newToyObject)
+      body: JSON.stringify({
+        name: event.target.name.value,
+        image: event.target.image.value,
+        likes: 0
+      })
     })
       .then(response => response.json())
       .then(newToyObject => {
-        console.log(newToyObject)
         renderNewToy(newToyObject)
       })
-    // .catch
-    // response.ok
   })
 
-
-  const toyCollectionDiv = document.querySelector('#toy-collection')
-  console.log(toyCollectionDiv)
   toyCollectionDiv.addEventListener('click', function (event) {
     if (event.target.matches('.like-btn')) {
       const cardDiv = event.target.closest('.card')
-      console.log(cardDiv)
       const id = cardDiv.dataset.id
       const pTag = cardDiv.querySelector('p')
-      const newLikes = parseInt(pTag.textContent) + 1
-      
+
       fetch(`${url}/${id}`, {
         method: 'PATCH',
         headers: {
           "Content-Type": "application/json",
           "Accept": "application/json"
         },
-        body: JSON.stringify({likes: newLikes})
+        body: JSON.stringify({ likes: parseInt(pTag.textContent) + 1 })
       })
-      .then(response => response.json())
-      .then(updatedToyObject => {
-        console.log(updatedToyObject)
-        pTag.textContent = `${updatedToyObject.likes} Likes`
-      })
+        .then(response => response.json())
+        .then(updatedToyObject => {
+          pTag.textContent = `${updatedToyObject.likes} Likes`
+        })
     }
   })
 
+
+  /*************** FUNCTIONS ************************/
+
+  function initialize() {
+    fetch(url)
+      .then(response => response.json())
+      .then(toys => {
+        toys.forEach(function (toyObject) {
+          renderNewToy(toyObject)
+        })
+      })
+  }
 
   function renderNewToy(toyObject) {
     // INNER HTML WAY
     const div = document.createElement('div')
     div.dataset.id = toyObject.id
-    // <div data-id='2'></div>
     div.className = 'card'
     div.innerHTML = `
         <h2>${toyObject.name}</h2>
@@ -114,8 +86,10 @@ document.addEventListener("DOMContentLoaded", () => {
         <p>${toyObject.likes} Likes </p>
         <button class="like-btn">Like <3</button>`
 
-    const toyCollection = document.querySelector("#toy-collection")
-    toyCollection.append(div)
+    toyCollectionDiv.append(div)
   }
+
+  /*************** INITIALIZE ************************/
+  initialize()
 
 });
